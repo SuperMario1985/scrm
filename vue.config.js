8bdd98daf4a9a7f317337a686a35e7f50620c8ba
lileilei <lileilei@idengyun.com>
2021-02-08 11:18:56 +0800

    feat: 添加dom自动更新

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,18 @@ module.exports = {
 	productionSourceMap: false,
 	css: {
 		// 是否使用css分离插件 ExtractTextPlugin
-        extract: IS_PROD,
-        // 开启 CSS source maps?
-        sourceMap: false,
-        // css预设器配置项
-        loaderOptions: {
-        },
-        // 启用 CSS modules for all css / pre-processor files.
-        modules: false,
+		extract: IS_PROD,
+		// 开启 CSS source maps?
+		sourceMap: false,
+		// css预设器配置项
+		loaderOptions: {
+		},
+		// 启用 CSS modules for all css / pre-processor files.
+		modules: false,
+	},
+	chainWebpack: config => {
+		// 修复HMR
+		config.resolve.symlinks(true);
 	},
 	configureWebpack: config => {
 		if (process.env.NODE_ENV === 'production') {
