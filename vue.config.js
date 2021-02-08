5b2e99383d114e3bf447f7979df8732a5def4ac8
lileilei <lileilei@idengyun.com>
2021-02-08 11:12:21 +0800

    feat: 样式热更新

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,19 @@
 const CompressionPlugin = require("compression-webpack-plugin")
+const IS_PROD = ['production', 'test'].includes(process.env.NODE_ENV)
 
 module.exports = {
 	assetsDir: 'assets',
 	productionSourceMap: false,
 	css: {
-		extract: false
+		// 是否使用css分离插件 ExtractTextPlugin
+        extract: IS_PROD,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {
+        },
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false,
 	},
 	configureWebpack: config => {
 		if (process.env.NODE_ENV === 'production') {
@@ -23,6 +32,7 @@ module.exports = {
 		//   errors: true
 		// },
 		host: 'localhost',
+		hot: true,
 		port: 8080,
 		https: false,
 		hotOnly: false,
