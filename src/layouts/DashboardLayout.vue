926ba831c69a147452bcddb5c669de6bef5efe4f
zhanglingqi <504024956@qq.com>
2021-02-08 11:34:01 +0800

    修改菜单背景色

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -14,7 +14,7 @@
 						theme="light"
 						mode="vertical"
 						:selectedKeys="[$route.name]"
-						:style="{borderRight: 0,top:0,bottom:'1px',left:0,right:0,position:'absolute',overflowY:'auto' }"
+						:style="{borderRight: 0,top:0,bottom:'1px',left:0,right:0,position:'absolute',overflowY:'auto',backgroundColor: '#f8f8f8'}"
 						@openChange="onOpenChange"
 						@click="onItemClick"
 				>
