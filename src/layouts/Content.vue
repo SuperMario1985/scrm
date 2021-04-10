e7eed6e964dd977f487e63aecd9588db4a10f712
lileilei <lileilei@idengyun.com>
2021-04-10 16:19:28 +0800

    feat:临时提交

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,28 +1,69 @@
 <template>
-	<a-layout>
-		<a-layout-content
-				:style="{ background: '#F1F1F1', margin: 0, minHeight: '280px', position: 'relative',height:'100%','overflow-x':'auto' }">
-			<keep-alive
-					:include="urlList">
-				<router-view>
-				</router-view>
-			</keep-alive>
-		</a-layout-content>
-	</a-layout>
+  <a-layout>
+    <a-layout-content
+      :style="{
+        background: '#F1F1F1',
+        margin: 0,
+        minHeight: '280px',
+        position: 'relative',
+        height: '100%',
+        'overflow-x': 'auto',
+      }"
+    >
+      <keep-alive :include="urlList">
+        <router-view> </router-view>
+      </keep-alive>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script>
-
-	export default {
-		name: "Content",
-		data () {
-			return {
-				urlList: ['customManageList', 'filingCabinetList', 'fissionList', 'fissionParticipants', 'raffleList', 'raffleParticipants', 'channelCodeList', 'welcomeList', 'massMessageList', 'smsSendList', 'smsTemplateList', 'fansList', 'replyList', 'pushList', 'sceneList', 'customerList', 'templateList', 'seniorList', 'redPacketEmployeeManagement', 'redPacketReconciliation', 'autoCreateList', 'customCrmKanban', 'channelCodeBaiduList', 'redFissionList', 'redFissionParticipants', 'customManageList', 'ViolationsNotice', 'groupList', 'tagsCreateList', 'highSeasCustomerList', 'wholeMarketFissionList', 'redForNewRule', 'redForNewList', 'wxWholeMarketFission','groupClockIn', 'customRecord']
-			}
-		}
-	}
+export default {
+  name: "Content",
+  data() {
+    return {
+      urlList: [
+        "customManageList",
+        "filingCabinetList",
+        "fissionList",
+        "fissionParticipants",
+        "raffleList",
+        "raffleParticipants",
+        "channelCodeList",
+        "welcomeList",
+        "massMessageList",
+        "smsSendList",
+        "smsTemplateList",
+        "fansList",
+        "replyList",
+        "pushList",
+        "sceneList",
+        "customerList",
+        "templateList",
+        "seniorList",
+        "redPacketEmployeeManagement",
+        "redPacketReconciliation",
+        "autoCreateList",
+        "customCrmKanban",
+        "channelCodeBaiduList",
+        "redFissionList",
+        "redFissionParticipants",
+        "customManageList",
+        "ViolationsNotice",
+        "groupList",
+        "tagsCreateList",
+        "highSeasCustomerList",
+        "wholeMarketFissionList",
+        "redForNewRule",
+        "redForNewList",
+        "wxWholeMarketFission",
+        "groupClockIn",
+        "customRecord",
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
